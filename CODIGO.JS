0be70f6104a0b6e6ff2ca9de8a72bb29ccc8bd72
faf1425 <126594204+faf1425@users.noreply.github.com>
2023-02-28 21:43:36 -0300

    Create CODIGO.JS

---
+++ CODIGO.JS
@@ -0,0 +1,4 @@
+document.getElementById(boton).addEventListener(click, function ()
+    {
+    console.log("GRACIAS POR EL DICTADO DEL CURSO! FALTA PROFUNDIZAR, PERO SIENTO QUE VOY POR BUEN CAMINO");
+})
